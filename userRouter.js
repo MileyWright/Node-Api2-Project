2ef426f6381263355d2faec16cf8109a6de1d779
Miley Wright <dishaliekwright@yahoo.com>
2020-01-19 01:54:51 -0500

    POST request for comments complete

--- userRouter.js
+++ userRouter.js
@@ -40,12 +40,18 @@ router.post('/:id/comments', (req,res) => {
     const id = req.params.id;
     const dbData = req.body;
     db.findById(id)
-    then(res => {
+    .then(comment => {
         if(!id){
            res.status(404).json({message: 'The post with the specified ID does not exist.'}) 
         } else if(!dbData.text){
             res.status(400).json({errorMessage: 'Please provide text for the comment.'})
+        } else {
+            res.status(201).json({ comment })
         }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ errorMessage: 'There was an error while saving the post to the database.'})
     })
 })
 
